e94901acef4d3775b47fa8718f871d57be1b0deb
Yannick Granger <yannick.gger@gmail.com>
2023-02-25 19:11:16 +0100

    add task ApiResource

---
+++ src/ApiResource/Task.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\ApiResource;
+
+use ApiPlatform\Metadata\ApiResource;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+
+/** A book. */
+#[ApiResource]
+#[ORM\Entity]
+class Task
+{
+    #[ORM\Id, ORM\Column, ORM\GeneratedValue]
+    private ?int $id;
+
+    #[ORM\Column]
+    private string $name;
+
+    #[ORM\Column(type: 'datetime_immutable')]
+    private \DateTimeImmutable $createdAt;
+
+    #[ORM\Column(type: 'datetime')]
+    private \DateTime $updatedAt;
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+        $this->createdAt = new \DateTimeImmutable();
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getCreatedAt(): \DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    public function getUpdatedAt(): \DateTime
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTime $updatedAt): void
+    {
+        $this->updatedAt = $updatedAt;
+    }
+}
