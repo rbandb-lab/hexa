5c3416423c14d012b86fc17f8bf597e95d5bae3b
Yannick Granger <yannick.granger@skilleos.com>
2023-02-28 12:37:49 +0100

    docker + bundles

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,6 @@ return [
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
