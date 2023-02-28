2b7b6ff361cab524d3a2bf8f24582f1abd1c6af8
Yannick Granger <yannick.granger@skilleos.com>
2023-02-28 13:27:27 +0100

    add migration

--- migrations/Version20230228122638.php
+++ migrations/Version20230228122638.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230225181414 extends AbstractMigration
+final class Version20230228122638 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,13 +20,16 @@ final class Version20230225181414 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE task (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL --(DC2Type:datetime_immutable)
-        , updated_at DATETIME NOT NULL)');
+        $this->addSql('CREATE SEQUENCE task_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE task (id INT NOT NULL, name VARCHAR(255) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, status VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('COMMENT ON COLUMN task.created_at IS \'(DC2Type:datetime_immutable)\'');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP SEQUENCE task_id_seq CASCADE');
         $this->addSql('DROP TABLE task');
     }
 }
--- migrations/Version20230228173646.php
+++ migrations/Version20230228173646.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230228123336 extends AbstractMigration
+final class Version20230228173646 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,22 +21,22 @@ final class Version20230228123336 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SEQUENCE status_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE SEQUENCE task_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE status (id INT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE TABLE task (id INT NOT NULL, status_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE INDEX IDX_527EDB256BF700BD ON task (status_id)');
-        $this->addSql('COMMENT ON COLUMN task.created_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('ALTER TABLE task ADD status_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE task DROP status');
         $this->addSql('ALTER TABLE task ADD CONSTRAINT FK_527EDB256BF700BD FOREIGN KEY (status_id) REFERENCES status (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('CREATE INDEX IDX_527EDB256BF700BD ON task (status_id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('DROP SEQUENCE status_id_seq CASCADE');
-        $this->addSql('DROP SEQUENCE task_id_seq CASCADE');
         $this->addSql('ALTER TABLE task DROP CONSTRAINT FK_527EDB256BF700BD');
+        $this->addSql('DROP SEQUENCE status_id_seq CASCADE');
         $this->addSql('DROP TABLE status');
-        $this->addSql('DROP TABLE task');
+        $this->addSql('DROP INDEX IDX_527EDB256BF700BD');
+        $this->addSql('ALTER TABLE task ADD status VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE task DROP status_id');
     }
 }
